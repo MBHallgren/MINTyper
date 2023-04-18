438d39c77f083e17bbd4277c133680e19ec1fc66
MBHallgren <s153002@student.dtu.dk>
2023-04-18 13:56:33 +0200

    dev14

--- mintyper/validateInput.py
+++ mintyper/validateInput.py
@@ -6,16 +6,17 @@ sys.path = [os.path.join(os.path.dirname(os.path.realpath(__file__)),'..')] + sy
 
 def validate_input(arguments):
     check_illumina_pe(arguments.illumina)
-    check_database(arguments.database)
+    check_database(arguments.database, arguments.reference)
 
 def check_illumina_pe(files):
     if len(files) % 2 != 0:
         logging.info('Error: Illumina files must be paired-end. Exiting.')
         sys.exit('Error: Illumina files must be paired-end. Exiting.')
 
-def check_database(database):
-    db_list = ['.name', '.comp.b', '.length.b', '.seq.b']
-    for db in db_list:
-        if not os.path.isfile(database + db):
-            logging.info('Error: KMA database not found. Exiting.')
-            sys.exit('Error: KMA database not found. Exiting.')
+def check_database(database, reference):
+    if reference == None:
+        db_list = ['.name', '.comp.b', '.length.b', '.seq.b']
+        for db in db_list:
+            if not os.path.isfile(database + db):
+                logging.info('Error: KMA database not found. Exiting.')
+                sys.exit('Error: KMA database not found. Exiting.')
