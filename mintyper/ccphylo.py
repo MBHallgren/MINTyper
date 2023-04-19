795b1841e2c8b7807460dfe5d03b28a85ee5b4b6
MBHallgren <s153002@student.dtu.dk>
2023-04-19 08:04:28 +0200

    dev22

--- mintyper/ccphylo.py
+++ mintyper/ccphylo.py
@@ -3,16 +3,12 @@ import sys
 import logging
 import subprocess
 
-class CcphyloRunner():
-    def __init__(self, target_dir, reference_header_text, ccphylo_flag, cluster_length):
+class CcphyloDist():
+    def __init__(self, target_dir):
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.INFO)
         self.check_for_ccphylo()
         self.target_dir = target_dir
-        self.reference_header_text = reference_header_text
-        self.ccphylo_flag = ccphylo_flag
-        self.cluster_length = cluster_length
-        self.prepare_list_of_alignment_files()
 
 
     def check_for_ccphylo(self):
@@ -22,18 +18,6 @@ class CcphyloRunner():
         except FileNotFoundError:
             self.logger.info("ccphylo is not installed correctly directly in the PATH.")
             sys.exit(1)
-            
-    def prepare_list_of_alignment_files(self):
-        """Returns a list of alignment files"""
-        run_list = []
-        fsa_list = os.listdir(self.target_dir + '/alignments/')
-        for item in fsa_list:
-            if item.endswith(".fsa"):
-                if os.path.getsize(self.target_dir + '/alignments/' + item) > 0: #non empty alignment
-                    run_list.append(self.target_dir + '/alignments/' + item)
-                else:
-                    logging.info('The alignment file {} is empty and therefore it was excluded from the analysis'.format(item))
-        self.alignment_string = " ".join(run_list)
 
     def dist(self):
         """runs ccphylo"""
@@ -47,12 +31,38 @@ class CcphyloRunner():
         self.logger.info("Running ccphylo with the following command: {}".format(cmd))
         os.system(cmd)
 
-    def dbscan(self):
-        """runs ccphylo dbscan"""
-        cmd = "ccphylo dbscan --input {}/distmatrix.txt --output {}/clusters.txt --min_samples 1 --eps {}" \
-            .format(self.target_dir, self.target_dir, self.cluster_length)
-        self.logger.info("Running ccphylo with the following command: {}".format(cmd))
-        os.system(cmd)
+
+class CcphyloTree():
+    def __init__(self, target_dir, reference_header_text, ccphylo_flag, cluster_length):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        self.check_for_ccphylo()
+        self.target_dir = target_dir
+        self.reference_header_text = reference_header_text
+        self.ccphylo_flag = ccphylo_flag
+        self.cluster_length = cluster_length
+        self.prepare_list_of_alignment_files()
+
+    def check_for_ccphylo(self):
+        """Checks if ccphylo is installed"""
+        try:
+            subprocess.call(["ccphylo"], stdout=open(os.devnull, 'wb'))
+        except FileNotFoundError:
+            self.logger.info("ccphylo is not installed correctly directly in the PATH.")
+            sys.exit(1)
+
+    def prepare_list_of_alignment_files(self):
+        """Returns a list of alignment files"""
+        run_list = []
+        fsa_list = os.listdir(self.target_dir + '/alignments/')
+        for item in fsa_list:
+            if item.endswith(".fsa"):
+                if os.path.getsize(self.target_dir + '/alignments/' + item) > 0:  # non empty alignment
+                    run_list.append(self.target_dir + '/alignments/' + item)
+                else:
+                    logging.info(
+                        'The alignment file {} is empty and therefore it was excluded from the analysis'.format(item))
+        self.alignment_string = " ".join(run_list)
 
     def tree(self):
         """runs ccphylo tree"""
@@ -61,7 +71,44 @@ class CcphyloRunner():
         self.logger.info("Running ccphylo with the following command: {}".format(cmd))
         os.system(cmd)
 
+class CcphyloDBSCAN():
+    def __init__(self, target_dir, reference_header_text, ccphylo_flag, cluster_length):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        self.check_for_ccphylo()
+        self.target_dir = target_dir
+        self.reference_header_text = reference_header_text
+        self.ccphylo_flag = ccphylo_flag
+        self.cluster_length = cluster_length
+        self.prepare_list_of_alignment_files()
 
+    def check_for_ccphylo(self):
+        """Checks if ccphylo is installed"""
+        try:
+            subprocess.call(["ccphylo"], stdout=open(os.devnull, 'wb'))
+        except FileNotFoundError:
+            self.logger.info("ccphylo is not installed correctly directly in the PATH.")
+            sys.exit(1)
+
+    def prepare_list_of_alignment_files(self):
+        """Returns a list of alignment files"""
+        run_list = []
+        fsa_list = os.listdir(self.target_dir + '/alignments/')
+        for item in fsa_list:
+            if item.endswith(".fsa"):
+                if os.path.getsize(self.target_dir + '/alignments/' + item) > 0:  # non empty alignment
+                    run_list.append(self.target_dir + '/alignments/' + item)
+                else:
+                    logging.info(
+                        'The alignment file {} is empty and therefore it was excluded from the analysis'.format(item))
+        self.alignment_string = " ".join(run_list)
+
+    def dbscan(self):
+        """runs ccphylo dbscan"""
+        cmd = "ccphylo dbscan --max_distance {} --input {}{} --output {}{}" \
+            .format(cluster_length, self.target_dir, '/distmatrix.txt', self.target_dir, '/clusters.txt')
+        self.logger.info("Running ccphylo with the following command: {}".format(cmd))
+        os.system(cmd)
 
 
 
