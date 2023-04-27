984de4674b326d362e050d002e20dde42035105f
MBHallgren <s153002@student.dtu.dk>
2023-04-27 09:27:18 +0200

    ccphylo flag3

--- scripts/local_install.py
+++ scripts/local_install.py
@@ -6,12 +6,12 @@ cmd = "git clone https://bitbucket.org/genomicepidemiology/kma.git"
 os.system(cmd)
 cmd = "cd kma && make"
 os.system(cmd)
-cmd = "cd .."
+cmd = "cp kma ~/bin/. && cd .."
 os.system(cmd)
 #ccphylo
 cmd = "git clone https://bitbucket.org/genomicepidemiology/ccphylo.git"
 os.system(cmd)
 cmd = "cd ccphylo && make"
 os.system(cmd)
-cmd = "cd .."
+cmd = "cp ccphylo ~/bin/. && cd .."
 os.system(cmd)
