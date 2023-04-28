03efa9d1bea921ebe35752f661dd4817581aa348
MBHallgren <s153002@student.dtu.dk>
2023-04-28 06:02:23 +0200

    readme3

--- scripts/install_dependencies.py
+++ scripts/install_dependencies.py
@@ -1,17 +1,19 @@
 import os
 from setuptools import setup
+from pathlib import Path
 
+home = str(Path.home())
 #Kma
 cmd = "git clone https://bitbucket.org/genomicepidemiology/kma.git"
 os.system(cmd)
 cmd = "cd kma && make"
 os.system(cmd)
-cmd = "cp -r kma* ~/bin/. && cd .."
+cmd = "cp kma {}/bin/kma && cd ..".format(home)
 os.system(cmd)
 #ccphylo
 cmd = "git clone https://bitbucket.org/genomicepidemiology/ccphylo.git"
 os.system(cmd)
 cmd = "cd ccphylo && make"
 os.system(cmd)
-cmd = "cp -r ccphylo ~/bin/. && cd .."
+cmd = "cp ccphylo {}/bin/ccphylo && cd ..".format(home)
 os.system(cmd)
