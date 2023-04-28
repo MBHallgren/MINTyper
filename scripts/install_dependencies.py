ccd378fe06a2041d5589de858c2b607327db0000
MBHallgren <s153002@student.dtu.dk>
2023-04-28 05:59:41 +0200

    readme2

--- scripts/install_dependencies.py
+++ scripts/install_dependencies.py
@@ -6,12 +6,12 @@ cmd = "git clone https://bitbucket.org/genomicepidemiology/kma.git"
 os.system(cmd)
 cmd = "cd kma && make"
 os.system(cmd)
-cmd = "cp kma ~/bin/. && cd .."
+cmd = "cp -r kma* ~/bin/. && cd .."
 os.system(cmd)
 #ccphylo
 cmd = "git clone https://bitbucket.org/genomicepidemiology/ccphylo.git"
 os.system(cmd)
 cmd = "cd ccphylo && make"
 os.system(cmd)
-cmd = "cp ccphylo ~/bin/. && cd .."
+cmd = "cp -r ccphylo ~/bin/. && cd .."
 os.system(cmd)
